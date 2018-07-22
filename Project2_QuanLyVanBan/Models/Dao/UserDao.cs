29876e19d5c574a67c6dbc33aa9dbb3823a92b4d
maivietson <33647912+maivietson@users.noreply.github.com>
2018-07-23 00:40:46 +0700

    Them chuc nang cho admin

--- Project2_QuanLyVanBan/Models/Dao/UserDao.cs
+++ Project2_QuanLyVanBan/Models/Dao/UserDao.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Models.EF;
+using PagedList;
 
 namespace Models.Dao
 {
@@ -23,9 +24,61 @@ namespace Models.Dao
             return entity.ID;
         }
 
+        public bool Update(User entity)
+        {
+            try
+            {
+                var user = db.Users.Find(entity.ID);
+                user.Name = entity.Name;
+                if (!string.IsNullOrEmpty(entity.Password))
+                {
+                    user.Password = entity.Password;
+                }
+                user.Phone = entity.Phone;
+                user.Email = entity.Email;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var user = db.Users.Find(id);
+                db.Users.Remove(user);
+                db.SaveChanges();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<User> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<User> model = db.Users.OrderByDescending(x => x.ID);
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.UserName.Contains(searchString) || x.Name.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.ID).ToPagedList(page, pageSize);
+        }
+
         public User GetByUserName(string userName)
         {
             return db.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+
+        public User ViewDetail(int id)
+        {
+            return db.Users.Find(id);
         }
 
         public int Login(string userName, string password)
---
+++ Project2_QuanLyVanBan/QuanLyVanBan/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using Models.Dao;
+using Models.EF;
+using QuanLyVanBan.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyVanBan.Areas.Admin.Controllers
+{
+    public class UserController : BaseController
+    {
+        // GET: Admin/User
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
+        {
+            var dao = new UserDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var user = new UserDao().ViewDetail(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+
+                var encryptorPass = Encryptor.MD5Hash(user.Password);
+                user.Password = encryptorPass;
+
+                long id = dao.Insert(user);
+                if (id > 0)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Add user successfull!!");
+                }
+            }
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+
+                if (!string.IsNullOrEmpty(user.Password))
+                {
+                    var encryptorPass = Encryptor.MD5Hash(user.Password);
+                    user.Password = encryptorPass;
+                }
+
+                var result = dao.Update(user);
+                if (result)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Update user successfull!!");
+                }
+            }
+
+            return View("Index");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new UserDao().Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
