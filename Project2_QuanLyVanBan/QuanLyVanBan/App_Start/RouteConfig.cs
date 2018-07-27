126fe8c28fcaeffc536d392ee276b2f81b87dc4c
maivietson <33647912+maivietson@users.noreply.github.com>
2018-07-27 21:32:15 +0700

    xay dung giao dien nguoi dung

--- Project2_QuanLyVanBan/QuanLyVanBan/App_Start/RouteConfig.cs
+++ Project2_QuanLyVanBan/QuanLyVanBan/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace QuanLyVanBan
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "QuanLyVanBan.Controllers" }
             );
         }
     }
